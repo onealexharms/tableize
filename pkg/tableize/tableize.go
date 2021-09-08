aab9b07b705304cbbfa5974e01b6c0a2dfd594e2
Jason Felice <jason.m.felice@gmail.com>
2021-09-08 08:04:40 -0400

    Fix structure

---
+++ pkg/tableize/tableize.go
@@ -0,0 +1,9 @@
+package tableize
+
+import (
+	"io"
+)
+
+func Tableize(in io.Reader, out io.Writer) error {
+	return nil
+}
--- pkg/tableize/tableize_test.go
+++ pkg/tableize/tableize_test.go
@@ -1,10 +1,9 @@
 package tableize
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func Test_the_tests_work(t *testing.T) {
-	assert.Equal(t, "hello", "hello")
+func TestItRuns(t *testing.T) {
+	Tableize(nil, nil)
 }
