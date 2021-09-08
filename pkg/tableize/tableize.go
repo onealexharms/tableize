b94e60b639990af9c2fa341c2d58a660b77d2bc5
Jason Felice <jason.m.felice@gmail.com>
2021-09-08 11:59:16 -0400

    Move records into type

--- pkg/tableize/tableize.go
+++ pkg/tableize/tableize.go
@@ -8,27 +8,22 @@ import (
 )
 
 type tableizer struct {
-	widths map[string]int
+	records []map[string]string
+	widths  map[string]int
 }
 
-func (t *tableizer) parseInput(in io.Reader) ([]map[string]string, error) {
+func (t *tableizer) parseInput(in io.Reader) error {
 	rawData, err := io.ReadAll(in)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	var data []map[string]string
-	err = yaml.Unmarshal(rawData, &data)
-	if err != nil {
-		return nil, err
-	}
-
-	return data, nil
+	return yaml.Unmarshal(rawData, &t.records)
 }
 
-func (t *tableizer) computeColumnWidths(data []map[string]string) {
+func (t *tableizer) computeColumnWidths() {
 	t.widths = make(map[string]int)
-	for _, record := range data {
+	for _, record := range t.records {
 		for field, value := range record {
 			if len(field) > t.widths[field] {
 				t.widths[field] = len(field)
@@ -70,17 +65,14 @@ func (t *tableizer) row(record map[string]string) string {
 
 func Tableize(in io.Reader, out io.Writer) error {
 	t := tableizer{}
-	data, err := t.parseInput(in)
-	if err != nil {
+	if err := t.parseInput(in); err != nil {
 		return err
 	}
-
-	t.computeColumnWidths(data)
+	t.computeColumnWidths()
 	fmt.Fprintf(out, "%s\n", t.header())
 	fmt.Fprintf(out, "%s\n", t.separator())
-	for _, record := range data {
+	for _, record := range t.records {
 		fmt.Fprintf(out, "%s\n", t.row(record))
 	}
-
 	return nil
 }
