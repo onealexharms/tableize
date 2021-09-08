bef15c15b8ab9f28894ceeea07b45e0045d7365b
Jason Felice <jason.m.felice@gmail.com>
2021-09-08 14:13:12 -0400

    Extract checkCase

--- pkg/tableize/tableize_test.go
+++ pkg/tableize/tableize_test.go
@@ -7,32 +7,34 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestFormatsSingleColumnData(t *testing.T) {
-	in := bytes.NewReader([]byte(`
-        - foo: hello, there
-        `))
-	out := bytes.NewBuffer([]byte{})
-	err := Tableize(in, out)
+func checkCase(t *testing.T, in string, out string) {
+	reader := bytes.NewReader([]byte(in[1:]))
+	writer := bytes.NewBuffer([]byte{})
+	err := Tableize(reader, writer)
 	assert.NoError(t, err)
-	assert.Equal(t, `foo
+	assert.Equal(t, out[1:], writer.String())
+}
+
+func TestFormatsSingleColumnData(t *testing.T) {
+	checkCase(t, `
+- foo: hello, there
+`, `
+foo
 ------------
 hello, there
-`, out.String())
+`)
 }
 
 func TestDeterministicallySortsMultipleColumns(t *testing.T) {
-	in := bytes.NewReader([]byte(`
-        - b: 1
-          e: 2
-          a: 3
-          d: 4
-          c: 5
-        `))
-	out := bytes.NewBuffer([]byte{})
-	err := Tableize(in, out)
-	assert.NoError(t, err)
-	assert.Equal(t, `a b c d e
+	checkCase(t, `
+- b: 1
+  e: 2
+  a: 3
+  d: 4
+  c: 5
+`, `
+a b c d e
 - - - - -
 3 1 5 4 2
-`, out.String())
+`)
 }
